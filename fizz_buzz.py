f302062a73cd7bd2f17b5fdc4e5b3614a229543f
modyharshit23 <56276314+modyharshit23@users.noreply.github.com>
2020-10-13 12:02:23 +0530

    Add a fizz_buzz algo (in python)
    https://github.com/topics/hacktoberfest2020
    https://github.com/topics/hacktoberfest
    hacktoberfest2020
    https://github.com/topics/hacktoberfest-accepted

---
+++ fizz_buzz.py
@@ -0,0 +1,15 @@
+def fizz_buzz(input):
+    if input % 3 == 0 and input % 5 == 0:
+        return("FizzBuzz")
+    elif input % 3 == 0:
+        return("Fizz")
+    elif input % 5 == 0:
+        return("Buzz")
+    else:
+        return(input)
+
+
+print(fizz_buzz(15))
+
+
+# return("fizz_buzz" if input % 3 == 0 and input % 5 == 0 elif input%3==0 :"fizz" elif input%5 ==0: "buzz" else input)
